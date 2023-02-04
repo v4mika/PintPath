3107dd3326c6b026266bc6970933030f2569b73f
jlt21 <jlt21@ac.ic.uk>
2023-02-04 15:47:04 +0000

    Get all pubs

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+ 
 export default {
   name: 'HelloWorld',
   // data is where we hold component level mutatable data.
@@ -26,13 +27,58 @@ export default {
       this.catInfo = await response.json();
     },
     async getCatFactAdvanced() {
+      console.log("Donwloading")
       // this example doesn't work as the API doesn't support it but this is what it may look like if you needed to pass data in the request
       // The user can enter a breed in the input we have in the template, which is then saved to "breed" in data and then we reference that here
-      const response = await fetch(
-        'https://api.openstreetmap.org/api/0.6/trackpoints?bbox=0,51.5,0.25,51.75'
+      /*const response = await fetch(
+        "https://nominatim.openstreetmap.org/search?q=&format=json"
         //`https://catfact.ninja/fact/breed/${this.breed}`
-      );
-      this.catInfo = await response;
+
+
+      );*/
+
+     
+
+      const query = `
+[out:json][timeout:25];
+(
+  node["amenity"="bar"](51.5074, -0.1278, 51.5200, -0.1150);
+  node["amenity"="pub"](51.5074, -0.1278, 51.5200, -0.1150);
+  way["amenity"="bar"](51.5074, -0.1278, 51.5200, -0.1150);
+  way["amenity"="pub"](51.5074, -0.1278, 51.5200, -0.1150);
+  relation["amenity"="bar"](51.5074, -0.1278, 51.5200, -0.1150);
+  relation["amenity"="pub"](51.5074, -0.1278, 51.5200, -0.1150);
+);
+out body;
+>;
+out skel qt;
+`;
+
+fetch("https://overpass-api.de/api/interpreter", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: query,
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+      /*
+      
+      const bbox = [37.769, 37.819, -122.406, -122.356];
+      const response = await fetch(`https://overpass-api.de/api/interpreter?data=[out:json];node[amenity=restaurant](bbox);out;`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
+      this.catInfo = await response.json();*/
     },
   },
 };
