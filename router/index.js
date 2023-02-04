cdb0c28de18bbc74be1be963cd31245d27debba0
elsa-polo <elsa.polo.laube@gmail.com>
2023-02-04 15:11:05 +0000

    initial

---
+++ router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+
+//  This is where we handle our seperate pages, Below are two examples of way's to create them.
+// you very cute and I love you very much
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
