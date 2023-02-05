c198385da5edff58547427be6b8054fc547228c4
jlt21 <jlt21@ac.ic.uk>
2023-02-05 11:53:12 +0000

    Remove extra pub crawl

--- pages/PubCrawl.vue
+++ pages/PubCrawl.vue
@@ -209,49 +209,7 @@
 
 
 
-<div class="px-4 sm:px-6 lg:px-8">
-    <div class="sm:flex sm:items-center">
-      <div class="sm:flex-auto">
-        <h1 class="text-xl font-semibold text-gray-900">Pub Golf</h1>
-        <p class="mt-2 text-sm text-gray-700">Once you have created your pub crawl route, why not have more fun and play a game of pub golf</p>
-      </div>
-      <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <button type="button" @click="createPubGolf" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Create Pub Golf</button>
-      </div>
-    </div>
-    
-  
-    <div class="mt-8 flex flex-col">
-      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-          <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-300">
-              <thead class="bg-gray-50">
-                <tr class="divide-x divide-gray-200">
-                  <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6">Hole</th>
-                  <th scope="col" class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Pub</th>
-                  <th scope="col" class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900">Drink</th>
-                  <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">Par</th>
-                  <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">Challenge</th>
-                  <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6">Score</th>
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-gray-200 bg-white">
-                <tr v-for="(pub, index) in pubgolf" :key="pub.name" class="divide-x divide-gray-200">
-                  <td class="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6">{{ index+1 }}</td>
-                  <td class="whitespace-nowrap p-4 text-sm text-gray-500">{{ pub.name }}</td>
-                  <td class="whitespace-nowrap p-4 text-sm text-gray-500">{{ pub.drink }}</td>
-                  <td class="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">{{ pub.par }}</td>
-                  <td class="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6">{{ pub.challenge }}</td>
-                  <td class="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6"></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+
 
 
 
