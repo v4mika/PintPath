1a0e3fb7b3374c2dc878c25cac2b7314b53bb347
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2023-02-05 09:41:54 +0000

    pub golf button takes you to pub golf page although we need to decide if the pub golf stuff will actually be a new page

--- pages/PubCrawl.vue
+++ pages/PubCrawl.vue
@@ -67,7 +67,9 @@
         </div>
       </div>
       <div>
-        <button v-if="pubs.length != 0" type="submit" class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Pub Golf!</button>
+        <a href="/pubgolf">
+          <button v-if="pubs.length != 0" type="submit" class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Pub Golf!</button>
+        </a>
       </div>
     </div>
      </fieldset>
